d8315cadd453cc447159976af541f05a07445bdb
joeru <joeru.dev@gmail.com>
2025-05-20 00:42:35 +1200

    init; rm malmo stuff

--- test_malmo.py
+++
@@ -1,5 +0,0 @@
-from malmoenv import Env
-xml = open('missions/test_pvp.xml').read()
-agent = Env()
-agent.init(xml=xml, port=9000, server='127.0.0.1', role=0)
-obs = agent.reset()    # should now work without assertion
